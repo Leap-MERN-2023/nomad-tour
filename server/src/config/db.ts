08eae790eabf4fd60ed4a1818677e8fd008493d8
Galsanpuntsag <145331408+Galsanpuntsag@users.noreply.github.com>
2024-03-15 14:58:20 +0800

    setup server and connected to DB (#10)
    Co-authored-by: User <user@Users-MacBook-Air.local>

---
+++ server/src/config/db.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import color from "colors";
+
+export const connectDB = async (uri: string) => {
+  try {
+    await mongoose.connect(uri);
+    console.log(color.bgGreen("Database is connected"));
+  } catch (error) {
+    console.log(color.bgRed("Database failed to connect"));
+    console.error(error);
+  }
+};
