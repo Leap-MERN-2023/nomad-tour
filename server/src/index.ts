9d88559b2c91a63395a25f8b7aca374da306307a
Билгүүн Баттөгс <bobbebe40@gmail.com>
2024-03-15 15:41:38 +0800

    backend created controller and router folder

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,11 @@ import express, { Application } from "express";
 import color from "colors";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db";
+import cors from "cors";
+
+import authRoutes from "./routes/authRoutes";
+import countryRoutes from "./routes/countryRoutes";
+import airportRoutes from "./routes/airportRoutes";
 
 dotenv.config();
 
@@ -10,11 +15,13 @@ const app: Application = express();
 const MONGODB_URI = process.env.MONGODB_URI as string;
 const port = process.env.PORT;
 
-console.log("MONGODB_URI: ", typeof MONGODB_URI);
-
 connectDB(MONGODB_URI);
 
 app.use(express.json());
+app.use(cors());
+app.use("/auth", authRoutes);
+app.use("/country", countryRoutes);
+app.use("/airport", airportRoutes);
 
 app.listen(port, () =>
   console.log(color.rainbow(`Server is listening ${port}`))
