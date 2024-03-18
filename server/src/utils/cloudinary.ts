e3baef3c3c632c1eb273ee09fe6d57fb6c336475
User <user@Users-MacBook-Air.local>
2024-03-18 11:41:03 +0800

    cloudinary install, uplouded image  and checked postman

---
+++ server/src/utils/cloudinary.ts
@@ -0,0 +1,16 @@
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+console.log("CLOUDNAME", process.env.CLOUD_NAME);
+console.log("CLOUDKEY", process.env.CLOUD_API_KEY);
+console.log("CLOUDSECRET", process.env.CLOUD_API_SECRET);
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
+export default cloudinary;
