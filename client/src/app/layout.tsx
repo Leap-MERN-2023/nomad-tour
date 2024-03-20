63d89a0daae5e88268762c477ea0f3a12699cc81
unknown <otgoohghg@yahoo.com>
2024-03-20 15:49:22 +0800

    airport context hiiw

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import AirPortProvider from "@/context/airportProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <AirPortProvider>
       <body className={inter.className}>{children}</body>
+      </AirPortProvider>
     </html>
   );
 }
