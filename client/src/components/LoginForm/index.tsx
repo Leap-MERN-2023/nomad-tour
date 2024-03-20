577fa7526f7ee5887d8afafa0399fb6f359a0614
Amaraa0505 <amarsanaa.tumurbaatar26@gmail.com>
2024-03-20 14:53:31 +0700

    login frontend

--- client/src/components/LoginForm/index.tsx
+++ client/src/components/LoginForm/index.tsx
@@ -1,26 +1,59 @@
 import React, { useState } from "react";
 
 export const LoginForm = ({ open, closeForm }: any) => {
-
-
   return (
-    <dialog open={open} className="modal bg-slate-200 w-64 flex mt-96 rounded-xl flex-col justify-center items-center my-3">
-      <div className="modal-box ">
+    <dialog
+      open={open}
+      className="modal bg-slate-200 w-96 flex h-96 rounded-xl my-36 mx-24"
+    >
+      <div className="modal-box flex flex-col gap-4 w-full h-full">
         <div className="flex justify-around">
-       <p className="text-blue-900 font-bold">Нэвтрэх / Бүртгүүлэх</p>
-       <button  onClick={closeForm} className="hover:text-black  text-gray-700">
-             X
-            </button>
-            </div>
-          <div className="flex flex-col gap-2">
-          <input type="text" placeholder="email" className="input input-bordered w-56 p-1 max-w-xs rounded-md" />
-          <input type="text" placeholder="password" className="input input-bordered w-56 p-1 max-w-xs rounded-md" />
-          </div>
-          <button className="btn btn-outline btn-success">Success</button>
-           
-           
-         
-        
+          <p className="text-blue-900 font-bold">Нэвтрэх / Бүртгүүлэх</p>
+          <button
+            onClick={closeForm}
+            className="hover:text-black  text-gray-700"
+          >
+            X
+          </button>
+        </div>
+
+        <input
+          type="text"
+          placeholder="email"
+          className="input input-bordered w-full p-3 max-w-xs rounded-md "
+        />
+        <input
+          type="text"
+          placeholder="password"
+          className="input input-bordered w-full p-3 max-w-xs rounded-md"
+        />
+        <div className="flex justify-end">
+          <button className="btn btn-outline btn-success">
+            Нууц үг мартсан
+          </button>
+        </div>
+        <button className="btn  btn-neutral">Үргэлжлүүлэх</button>
+        <div className="divider divider-neutral text-black">OR</div>
+        <div className="flex justify-between">
+          <button className="border-[1px]  w-24 p-3 hover:bg-gray-200 rounded-md flex justify-center">
+            <img
+              src="https://cdn.icon-icons.com/icons2/730/PNG/512/gmail_icon-icons.com_62758.png"
+              className="w-6 rounded-full"
+            />
+          </button>
+          <button className="border-[1px] w-24 p-3 hover:bg-gray-200 rounded-md flex justify-center">
+            <img
+              src="https://www.cabriniclinic.org/wp-content/uploads/2014/02/facebook-icon.png"
+              className="w-6 rounded-full"
+            />
+          </button>
+          <button className="border-[1px] w-24 p-3 hover:bg-gray-200 rounded-md flex justify-center">
+            <img
+              src="https://static.vecteezy.com/system/resources/previews/018/930/695/original/twitter-logo-twitter-icon-transparent-free-free-png.png"
+              className="w-6 rounded-full"
+            />
+          </button>
+        </div>
       </div>
     </dialog>
   );
---
+++ client/src/context/UserProvider/inedx.tsx
@@ -0,0 +1,114 @@
+import axios from "axios";
+import {
+  useState,
+  useEffect,
+  createContext,
+  PropsWithChildren,
+  useContext,
+  ChangeEvent,
+} from "react";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
+
+export interface IUser {
+  name: string;
+  email: string;
+  phoneNumber: string;
+}
+
+export interface IUserContext {
+  user: IUser;
+  token: string | null;
+  login: (email: string, password: string) => void;
+}
+
+export const UserContext = createContext<IUserContext>({
+  user: {
+    name: "",
+    email: "",
+    phoneNumber: "",
+  },
+  token: "",
+  login: function () {},
+});
+
+export const UserProvider = ({ children }: PropsWithChildren) => {
+  const router = useRouter();
+
+  const [newUser, setNewUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    phoneNumber: "",
+  });
+
+  const handleChangeUser = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setNewUser({ ...newUser, [name]: value });
+  };
+
+  const [token, setToken] = useState<string | null>(null);
+  const [user, setUser] = useState<IUser>({
+    name: "",
+    email: "",
+    phoneNumber: "",
+  });
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token");
+    if (storedToken) {
+      setToken(storedToken);
+    }
+  }, []);
+
+  const login = async (email: string, password: string) => {
+    try {
+      const { data } = await axios.post("http://localhost:8008/auth/login", {
+        email: email,
+        password: password,
+      });
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("user", JSON.stringify(data.user));
+      setUser(data.user);
+      setUser(data.token);
+      router.push("/");
+      toast("amjilttai nevterlee");
+    } catch (error) {
+      toast.error("nevtrelt amjiltgui");
+    }
+  };
+
+  const signup = async (
+    email: string,
+    password: string,
+    name: string,
+    phoneNumber: string
+  ) => {
+    try {
+      const formData = new FormData();
+      formData.set("name", newUser.name);
+      formData.set("email", newUser.email);
+      formData.set("password", newUser.password);
+      formData.set("phoneNumber", newUser.phoneNumber);
+
+      const token = localStorage.getItem("token");
+
+      await axios.post("http://localhost:8008/auth/signup"),
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        };
+    } catch (error: any) {
+      alert("Error" + error.message);
+    }
+  };
+
+  return (
+    <UserContext.Provider value={{ login, user, token }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
