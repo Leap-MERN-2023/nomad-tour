f593374b83c5e3c6a0b34fb3785a936ceeff639f
Галсанпунцаг Бат-Орших <LP230092@G304-CC0029.local>
2024-03-22 12:21:51 +0800

    style edited

--- client/src/components/country/index.tsx
+++ client/src/components/country/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 
 import explore from "./explore";
 import additionImage from "./additionImage";
@@ -12,6 +12,8 @@ import { CountryContext } from "@/context/CountryProvider";
 import { Autoplay } from "swiper/modules";
 
 import { gsap } from "gsap";
+
+
 
 const Country = () => {
   const { countries } = useContext(CountryContext);
@@ -33,11 +35,11 @@ const Country = () => {
               src={country?.images[0]}
               alt=""
             />
-            <div className="absolute top-40 text-4xl text-white font-extrabold left-16 border-white border-b-2 w-2/3 gap-2">
+            <div className="absolute top-48 md:flex md:w-full md:top-[330px] md:text-6xl text-4xl text-white font-extrabold left-16 border-white border-b-2 w-2/3 gap-2">
               Explore
-              <h1>{country.name}'</h1>
+              <h1>{country.name}</h1>
             </div>
-            <div className="absolute  top-64 text-white left-16  w-2/3 h-44 overflow-hidden p-2 rounded-lg items-center shadow-2xl ">
+            <div className="absolute  top-72 md:top-[420px] text-white left-16  w-2/3 h-44 md:w-1/4  overflow-hidden p-2 rounded-lg items-center shadow-2xl  md:py-3  md:h-64">
               {country.description}
             </div>
             <div className="hidden">
@@ -45,7 +47,8 @@ const Country = () => {
               <img src={country?.images[2]} alt="" />
               <img src={country?.images[3]} alt="" />
             </div>
-            <button className="btn btn-outline btn-accent">Accent</button>
+            <button className=" absolute border rounded-sm hover:outline shadow-lg bottom-80  sm:bottom-20 md:bottom-72 p-2 h-10 ml-16 text-white">See all..</button>
+
           </SwiperSlide>
         </div>
       ))}
