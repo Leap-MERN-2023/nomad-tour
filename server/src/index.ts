4239c1ad19244cc88ab4303342f13f71b50d475c
Амарсанаа Төмөрбаатар <LP230107@G301-CC0198.local>
2024-04-11 15:24:03 +0800

    zassan

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,15 +22,11 @@ const MONGODB_URI = process.env.MONGODB_URI ?? "";
 const PORT = process.env.PORT ?? "";
 const app: Application = express();
 
-
-
 app.use(cors());
 
 connectDb(MONGODB_URI);
 app.use(cors());
 app.use(express.json());
-
-
 
 app.use("/verify", verifyRoute);
 app.use("/airport", airportRoutes);
@@ -46,8 +42,5 @@ app.use("/ticket", ticketRoutes);
 app.use("/order", orderRoute);
 app.use("/flightOrder", flighOrderRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
 
 app.listen(PORT, () => console.log("Server is running at " + PORT));
